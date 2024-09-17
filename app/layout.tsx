e39c22613b28fe82db985eeff5793e2ac9eb34d6
Kunal Agrawal <kunal.py3@gmail.com>
2024-09-17 17:00:14 +0530

    Add caveat font style.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Caveat } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], weight: "variable" });
+const caveat = Caveat({ subsets: ["latin"]  , variable: "--font-caveat"});
 
 export const metadata: Metadata = {
   title: "Kunal Agrawal",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className+" select-none"}>{children}</body>
+      <body className={inter.className + " " + caveat.variable +" select-none"}>{children}</body>
     </html>
   );
 }
