a158b3ef3f30e585b45dc11ede631a8b7ccbe2fe
Kunal Agrawal <kunal.py3@gmail.com>
2024-10-03 22:13:35 +0530

    Add Sample data for heirarchy data for skills.

---
+++ components/Skill/data.ts
@@ -0,0 +1,80 @@
+const data = {
+  name: "Full Stack Web Developer",
+  value: 200,
+  children: [
+    {
+      name: "Frontend",
+      value: 200,
+      children: [
+        { name: "React", value: 100 },
+        { name: "Angular", value: 100 },
+        { name: "Vue", value: 100 },
+      ],
+    },
+    {
+      name: "Backend",
+      value: 200,
+      children: [
+        { name: "Node.js", value: 100 },
+        { name: "Express", value: 100 },
+        { name: "Django", value: 100 },
+      ],
+    },
+    {
+      name: "Database",
+      value: 200,
+      children: [
+        { name: "MongoDB", value: 100 },
+        { name: "MySQL", value: 100 },
+        { name: "PostgreSQL", value: 100 },
+      ],
+    },
+    {
+      name: "Cloud",
+      value: 200,
+      children: [
+        { name: "Firebase", value: 100 },
+        { name: "AWS", value: 100 },
+        { name: "Google Cloud", value: 100 },
+      ],
+    },
+    {
+      name: "DevOps",
+      value: 200,
+      children: [
+        { name: "Docker", value: 100 },
+        { name: "Kubernetes", value: 100 },
+        { name: "Jenkins", value: 100 },
+      ],
+    },
+    {
+      name: "Version Control",
+      value: 200,
+      children: [
+        { name: "Git", value: 100 },
+        { name: "GitHub", value: 100 },
+        { name: "GitLab", value: 100 },
+      ],
+    },
+    {
+      name: "Development Tools",
+      value: 200,
+      children: [
+        { name: "VS Code", value: 100 },
+        { name: "Postman", value: 100 },
+        { name: "Insomnia", value: 100 },
+      ],
+    },
+    {
+      name: "Testing",
+      value: 200,
+      children: [
+        { name: "Jest", value: 100 },
+        { name: "Mocha", value: 100 },
+        { name: "Selenium", value: 100 },
+      ],
+    },
+  ],
+};
+
+export default data;
