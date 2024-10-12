403f7708424e6e060b13c3ee99d594a95e2001c1
Kunal Agrawal <kunal.py3@gmail.com>
2024-10-12 12:34:14 +0530

    Add Skill component

--- components/Skill/index.tsx
+++ components/Skill/index.tsx
@@ -1,14 +1,18 @@
 import { SkillId } from "@/config/sectionId";
+import TreeMap from "./TreeMap";
+import data from "./data";
 
 function SkillSectionLayout() {
   return (
     <section id={SkillId} className="w-full relative mt-10">
       <div className="max-w-4xl md:mx-auto mx-2 z-10 backdrop-blur-3xl">
         <h2 className="text-center text-3xl md:text-4xl font-bold">
-          Work Experience
+          Skills
         </h2>
         <div className="h-[1px] w-full bg-gray-700 my-2 rounded-full"></div>
-        <div className="my-4"></div>
+        <div className="my-4">
+          <TreeMap data={data} />
+        </div>
       </div>
     </section>
   );
